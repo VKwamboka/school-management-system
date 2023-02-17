4ff87e1ec1df1bf839ca0a617389a6c717bc9582
Verah Nyagaka <verahnyagaka101@gmail.com>
2023-02-17 16:45:04 +0300

    updated code

--- src/app/student-details/student-details.component.ts
+++ src/app/student-details/student-details.component.ts
@@ -11,8 +11,6 @@ export class StudentDetailsComponent {
   @Output() sendData = new EventEmitter<{name:string, course:string, fee:string, balance:string}>
    AddStudent(name:HTMLInputElement, course:HTMLInputElement, fee:HTMLInputElement, balance:HTMLInputElement){
     this.sendData.emit({name: name.value, course: course.value, fee: fee.value, balance: balance.value})
-    // this.data.push({
-    //   name: name.value, course: course.value, fee: fee.value, balance: balance.value
-    // })
+
   }
 }
